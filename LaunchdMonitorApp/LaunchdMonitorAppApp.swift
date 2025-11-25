dd06e7419522573ec84904d09a8bb0fc574945fe
thomas <vuongdaonghe@yahoo.com>
2025-11-25 13:02:20 +0800

    Initial Commit

---
+++ LaunchdMonitorApp/LaunchdMonitorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchdMonitorAppApp.swift
+//  LaunchdMonitorApp
+//
+//  Created by macpro on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LaunchdMonitorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LaunchdMonitorAppTests/LaunchdMonitorAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchdMonitorAppTests.swift
+//  LaunchdMonitorAppTests
+//
+//  Created by macpro on 25/11/25.
+//
+
+import Testing
+@testable import LaunchdMonitorApp
+
+struct LaunchdMonitorAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
